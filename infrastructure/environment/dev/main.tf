b4bb39ec5e0d62fb59a25aa043ee210242db6fa0
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-04 21:43:11 +0530

    Create main.tf

---
+++ infrastructure/environment/dev/main.tf
@@ -0,0 +1,17 @@
+module "iam" {
+  source = "../../Modules/IAM"
+  
+}
+
+module "ecr" {
+  source    = "../../Modules/ECR"
+}
+
+
+
+module "lamda" {
+  source = "../../Modules/LAMDA"
+  lambda_role_arn = module.iam.lambda_role_arn
+  attach_basic_execution = module.iam.attach_basic_execution
+  lambda_services = var.lambda_services
+}
