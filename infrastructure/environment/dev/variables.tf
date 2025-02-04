fb4269e741525064f5fbe77365cf3b12e12ecdfa
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-04 21:46:38 +0530

    Create variables.tf

---
+++ infrastructure/environment/dev/variables.tf
@@ -0,0 +1,11 @@
+variable "patient_service_image_uri" {
+  description = "ECR image URI for Patient Service"
+  type        = string
+  default     = "575108922676.dkr.ecr.us-west-1.amazonaws.com/patient-service-repo:latest"
+}
+
+variable "appointment_service_image_uri" {
+  description = "ECR image URI for Appointment Service"
+  type        = string
+  default     = "575108922676.dkr.ecr.us-west-1.amazonaws.com/appointment-service-repo:latest"
+}
