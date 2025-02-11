ae9d48c687fcba53d36bd5905b6ccec4f7b1d9bf
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-11 13:38:41 +0530

    Update main.tf

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -1,13 +1,3 @@
-resource "aws_ecr_repository" "patient_service" {
-  name = "patient-service-repo"
-  tags = {
-    Name = "Patient Service Repository"
-  }
-}
-
-resource "aws_ecr_repository" "appointment_service" {
-  name = "appointment-service-repo"
-  tags = {
-    Name = "Appointment Service Repository"
-  }
+resource "aws_ecr_repository" "app_repo" {
+  name = var.repo_name
 }
