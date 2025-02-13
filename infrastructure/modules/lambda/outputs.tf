16120883360b6a25d677e1e403311bbc6d98d9d2
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-13 16:54:56 +0530

    Update outputs.tf

--- infrastructure/modules/lambda/outputs.tf
+++ infrastructure/modules/lambda/outputs.tf
@@ -2,7 +2,7 @@ output "api_gateway_url" {
   value = aws_apigatewayv2_stage.lambda_stage.invoke_url
 }
 
-output "api_gateway_url" {
+output "api_gateway_url1" {
   value = aws_apigatewayv2_api.lambda_api.api_endpoint
 }
 
