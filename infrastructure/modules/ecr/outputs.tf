d66dd31838fce639eff974a7e3097313ca49ccdc
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-13 14:47:17 +0530

    Update outputs.tf

--- infrastructure/modules/ecr/outputs.tf
+++ infrastructure/modules/ecr/outputs.tf
@@ -5,3 +5,7 @@
 output "appointment_service_repo_uri" {
   value = aws_ecr_repository.appointment_service.repository_url
 }*/
+
+output "ecr_repository_url" {
+value = aws_ecr_repository.sso_repo.repository_url
+}
